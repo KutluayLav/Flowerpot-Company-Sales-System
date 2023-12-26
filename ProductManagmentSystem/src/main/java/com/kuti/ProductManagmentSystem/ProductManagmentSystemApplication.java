26f06305cdc77d3e38bbab3e61d1ccbe6b9b7ead
Kutluay <kutluay3344@hotmail.com>
2023-12-27 02:44:50 +0300

    ProductManagmentSystem yüklendi

---
+++ ProductManagmentSystem/src/main/java/com/kuti/ProductManagmentSystem/ProductManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kuti.ProductManagmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagmentSystemApplication.class, args);
+	}
+
+}
