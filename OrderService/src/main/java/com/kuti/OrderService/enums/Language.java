bdc22a5b91df955200b33de8123e0c2679778043
Kutluay <kutluay3344@hotmail.com>
2024-01-08 16:33:25 +0300

    exception handler utils added

---
+++ OrderService/src/main/java/com/kuti/OrderService/enums/Language.java
@@ -0,0 +1,6 @@
+package com.kuti.OrderService.enums;
+
+public enum Language {
+    EN,TR;
+
+}
---
+++ OrderService/src/main/java/com/kuti/OrderService/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.kuti.OrderService.exception.handler;
+
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
