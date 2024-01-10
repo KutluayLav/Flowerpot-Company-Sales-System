b32ceea2812fb29c4c43a33a937fb817ae76bac9
Kutluay <kutluay3344@hotmail.com>
2024-01-10 11:06:37 +0300

    feat:order model added

---
+++ OrderService/src/main/java/com/kuti/OrderService/model/Order.java
@@ -0,0 +1,21 @@
+package com.kuti.OrderService.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class Order {
+
+    @Id
+    private long id;
+
+    private String name;
+}
